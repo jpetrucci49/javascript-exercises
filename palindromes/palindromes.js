bcf18e2acfdc8dc9703f44fc837981074b362767
jpetrucci <jpetrucci49@yahoo.com>
2019-03-01 16:36:15 -0500

    Palindromes complete - Compare string to it's reverse, return truthy falsy

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+    let filteredStr = str.toLowerCase().match(/[a-z]/ig)
+    return filteredStr.join('') === filteredStr.reverse().join('')
 }
 
 module.exports = palindromes
