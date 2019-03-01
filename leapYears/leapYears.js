6fa35f0525f3b67c46fcfb44f8190c0f189adaea
jpetrucci <jpetrucci49@yahoo.com>
2019-03-01 14:46:07 -0500

    leapYear test complete - returns true if provided year is a leap Year. false otherwise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // running truthy falsy tests on the year,
+    // return the result of the 2 conditions
+    // if both sides of && evaluate to true, returns true.
+    return (year % 4 === 0 && (year % 100 != 0)) || (year % 400 == 0)
 }
 
 module.exports = leapYears
