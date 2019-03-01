299851250e0bc32c9814067d662014ddeca6fbb6
jpetrucci <jpetrucci49@yahoo.com>
2019-03-01 18:43:47 -0500

    Pig Latin complete - uses regex to match first and second segments and joins

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,7 +1,17 @@
-function translate() {
-	// body...
+function translate(str) {
+	const words = str.split(' ').map(word => {
+		// use regex capture groups to select the start and end of a split
+		// first match to capture is 'qu'. if not present, match remaining constants
+		// second capture is whatever remains after the first
+		const match = word.match(/^((?:qu|[bcdfghjklmnpqrstvwxz])*)(.*)$/)
+		// match returns an array containing the initial word at index 0
+		// then first segment before the split at index 1
+		// then the second segment after the split at index 2
+		// return the second segment + the first + ay for each word
+		return match[2] + match[1] + "ay"
+	})
+	return words.join(' ')
 }
-
 
 module.exports = {
 	translate
