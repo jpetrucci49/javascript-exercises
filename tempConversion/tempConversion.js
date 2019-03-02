864b54aa7ab67f134b51db2f9a8c4dd0bb092d94
jpetrucci <jpetrucci49@yahoo.com>
2019-03-01 21:42:34 -0500

    temp Converter complete - Returns Temp rounded down to one decimal or flattened

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const result = ((temp - 32) * (5/9)).toFixed(1) 
+  return Number(result)
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const result = ((temp * (9/5)) + 32).toFixed(1)
+  return Number(result)
 }
 
 module.exports = {
