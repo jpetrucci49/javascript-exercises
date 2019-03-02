17cce3d078fac42d353a788c5b39a10b645c9385
jpetrucci <jpetrucci49@yahoo.com>
2019-03-01 20:56:13 -0500

    snake_case complete - parses different text cases to be snake_case

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,15 @@
-const snakeCase = function() {
-
+const snakeCase = function(str) {
+    str = str.replace(/\.+/g, ' ')
+    if (str.indexOf(' ') < 0) {
+        str = str.replace(/([A-Z])/g, ' $1')
+    }
+    return str
+        .trim()
+        .toLowerCase()
+        .replace(/[,\?]/g, '')
+        .replace(/\-/g, ' ')
+        .split(' ')
+        .join('_')
 }
 
 module.exports = snakeCase
