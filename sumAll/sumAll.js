72125440152b5ddea65c6924d44df633d7a1c9a1
jpetrucci <jpetrucci49@yahoo.com>
2019-03-01 21:21:11 -0500

    sumAll complete - Sums all integers between 2 provided inclusively

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    for (let i = 0; i < args.length; i++) {
+        if (typeof args[i] !== 'number'|| args[i] < 0) {
+            return 'ERROR'
+        }
+    }
+    let [ max, min ] = [ Math.max.apply(null, args), Math.min.apply(null, args) ]
+    let result = 0
+    while (min < max){
+        result += min
+        result += max
+        min++
+        max--
+    }
+    if (min === max){
+        result += min
+    }
+    return result
 }
 
 module.exports = sumAll
